42f7d0d30a134a15f80eb92edca1bd687a40b6b5
need Ubr <hun1387s@gmail.com>
2021-12-20 23:24:09 +0900

    first commit

---
+++ simsim.py
@@ -0,0 +1,17 @@
+print('Hello World!!')
+print('*'*15)
+print('Hello Python!!!')
+print('*'*15)
+print('Hello PyCharm!!!')
+print('*'*15)
+
+print('Hello World!!!')
+print('*' * 15)
+print('Hello Python!!!')
+print('*' * 15)
+print('Hello PyCharm!!!')
+print('*' * 15)
+
+
+
+# install python
